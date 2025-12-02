c54ff2556b3eaa8a9403d03acd76a073babf2339
Thanaphong Songsisai <9sepofficial@gmail.com>
2025-12-02 10:53:11 +0700

    feat: ✨ CS25-646 : Favorite Dx Phase3

--- src/dto/dischargeSummary/index.ts
+++ src/dto/dischargeSummary/index.ts
@@ -10,3 +10,5 @@ export enum DiagnosisProcedureType {
     OtherProcedure = "OtherProcedure",
     OtherNonOperation = "OtherNonOperation"
 }
+
+export * from "./utilityBox/auditorSuggestion";
---
+++ src/dto/dischargeSummary/utilityBox/auditorSuggestion.ts
@@ -0,0 +1,25 @@
+import type { DiagnosisProcedureType } from "..";
+
+export interface AuditorSuggestionResponse {
+    id: string;
+    name: string;
+    type: DiagnosisProcedureType;
+    icd?: AuditorSuggestionICD[] | null;
+    order?: number | null;
+    gender?: AuditorSuggestionGender;
+    ward?: string[] | null;
+    note?: string | null;
+    snomedId?: string | null;
+}
+
+export interface AuditorSuggestionICD {
+    id: string;
+    term?: string;
+    code: string;
+}
+
+export enum AuditorSuggestionGender {
+    male = "male",
+    female = "female",
+    both = "both"
+}
