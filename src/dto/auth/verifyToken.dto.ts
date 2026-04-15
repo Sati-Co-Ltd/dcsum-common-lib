24e2013a49718d8dfcdf169581ef07727a9a12e3
Tanapat Kahabodeekanokkul <pahntanapat@gmail.com>
2026-04-15 15:24:43 +0700

    feat: add DTO for auth

---
+++ src/dto/auth/verifyToken.dto.ts
@@ -0,0 +1,46 @@
+type OtaBmsUserInfo = {
+    name: string;
+    isCoder: boolean;
+    isDoctor: boolean;
+    userHisId: string;
+    isAuditStaff: boolean;
+    isAuditDoctor: boolean;
+    userLicenseNumber: string;
+};
+
+export type VerifyTokenOtaBmsResponse = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    license?: string;
+    hisId?: string;
+    roleName?: string;
+    moduleAction: number[];
+    roleId: string;
+    type: "ota" | "bms";
+    hospitalNumber?: string;
+    transactionNumber?: string;
+    user?: OtaBmsUserInfo;
+    token: string;
+    ota: boolean;
+    bms: boolean;
+    satiEcoSystemToken?: string | null;
+};
+
+export type VerifyTokenUserResponse = {
+    id: string;
+    license: string | null;
+    username: string;
+    firstName: string | null;
+    lastName: string | null;
+    roleId: string | null;
+    image: string | null;
+    userId: string | null;
+    hisId: string | null;
+    roleName: string;
+    moduleAction: number[];
+    token: string;
+    satiEcoSystemToken?: string | null;
+};
+
+export type VerifyTokenResponse = VerifyTokenOtaBmsResponse | VerifyTokenUserResponse;
