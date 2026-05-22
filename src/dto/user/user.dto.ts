8c6080e2849984f46b6b0c27aeaec76fcd166ad6
Thanaphong.s <9sepofficial@gmail.com>
2026-05-22 12:02:57 +0700

    feat: add UserDto, CreateUserDto, and UpdateUserDto interfaces

---
+++ src/dto/user/user.dto.ts
@@ -0,0 +1,27 @@
+export interface UserDto {
+    id: string;
+    firstName: string;
+    lastName: string;
+    image?: string | null;
+    license?: string | null;
+    username: string;
+    password: string;
+    certifiedNumber?: string | null;
+    roleId: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+
+    isActive?: boolean;
+    isLock?: boolean;
+    lockedAt?: Date | null;
+    isDelete?: boolean;
+    deletedAt?: Date | null;
+}
+
+export interface CreateUserDto extends Omit<UserDto, "id"> {
+    id?: string;
+}
+
+export interface UpdateUserDto extends Omit<UserDto, "password"> {
+    password?: string;
+}
